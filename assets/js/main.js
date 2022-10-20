8f24c2fe1fde5d4f8f5517297ef9b370ff04744e
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-10-20 14:17:58 +0200

    Added yesterday code. Added Bootstrap links.

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+function totalPriceTravel(){
+
+    // Costante prezzo chilometri
+
+    const kilometers = 0.21;
+
+    // Variabile numero chilometri che deve fare l'utente
+
+    let valueKm = document.getElementById('numbersKm').value;
+
+    // Variabile età che ha l'utente
+
+    let valueAge = document.getElementById('age').value;
+
+    let valueNameSurname = document.getElementById('nameSurname').value;
+
+    // Calcolo costo senza sconti al chilometro
+
+    let price = valueKm * kilometers;
+
+    // Calcolo costo sconto minorenne al chilometro
+
+    let under = price * 0.2;
+
+    const childPrice = price - under;
+
+    // Calcolo costo sconto anziano al chilometro
+
+    let over = price * 0.4;
+
+    const seniorPrice = price - over;
+
+
+
+    if (valueAge < 18) {
+        price = childPrice.toFixed(2)
+    }
+    else if (valueAge >= 65) {
+        price = seniorPrice.toFixed(2)
+    } 
+    else {
+        price = price.toFixed(2)
+    }
+
+    document.getElementById('totalKm').innerHTML = "Il costo totale del tuo percorso:" + " " + price + "€"
+
+
+
+}
